71a161df0bbaa929afb4ea61e871b00e1f595a40
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-03-12 17:41:21 +0100

    blop

--- custom.js
+++ custom.js
@@ -18,24 +18,38 @@ function waitForElm(selector) {
   });
 }
 
-const selector = "#lu_map"
+function getAddressURL() {
+  const search = document.querySelector("#APjFqb")?.textContent;
+  const address = search.split(' ').join('+');
+  return `https://www.google.fr/maps/search/${address}`;
+}
+
+const selector = "#lu_map";
+const selector2 = "[jsname='tRarif'][jsaction='click:ivJHQ']";
+
 
 waitForElm(selector).then((elm) => {
   elm.style.cursor = 'pointer';
   
-  const search = document.querySelector("#APjFqb")?.textContent
-  const address = search.split(' ').join('+');
-  const newUrl = `https://www.google.fr/maps/search/${address}`
-
   if (elm.closest("a")) {
-    elm.closest("a").href = newUrl
+    elm.closest("a").href = getAddressURL();
   } else {
     const imgEl = elm.querySelector('#pimg_1');
     const aElement = document.createElement('a');
-    aElement.href = newUrl;
+    aElement.href = getAddressURL();
     aElement.innerHTML = imgEl.parentElement.innerHTML;
 
     imgEl.replaceWith(aElement);
   }
+});
+
+waitForElm(selector2).then((elm) => {
+  const aElement = document.createElement('a');
+
+  aElement.classList.add('new-open-maps-btn');
+  aElement.href = getAddressURL();
+  aElement.innerHTML = 'Open in Maps';
+
+  elm.parentElement.appendChild(aElement);
 });
 
