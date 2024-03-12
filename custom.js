4b7558139754c9eb1fdc45df895909283ad6e370
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-03-12 15:06:08 +0100

    boom

--- custom.js
+++ custom.js
@@ -22,13 +22,20 @@ const selector = "#lu_map"
 
 waitForElm(selector).then((elm) => {
   elm.style.cursor = 'pointer';
-
-  const search = elm.title.split(': carte')[0];
+  
+  const search = document.querySelector("#APjFqb")?.textContent
   const address = search.split(' ').join('+');
   const newUrl = `https://www.google.fr/maps/search/${address}`
 
-  if (elm) {
+  if (elm.closest("a")) {
     elm.closest("a").href = newUrl
+  } else {
+    const imgEl = elm.querySelector('#pimg_1');
+    const aElement = document.createElement('a');
+    aElement.href = newUrl;
+    aElement.innerHTML = imgEl.parentElement.innerHTML;
+
+    imgEl.replaceWith(aElement);
   }
 });
 
