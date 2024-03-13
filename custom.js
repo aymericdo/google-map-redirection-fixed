5f61e8c61af4e36ccaedff212d8053564b7a4a5c
Aymeric Dominique <aymeric.dominique@gmail.com>
2024-03-13 18:33:18 +0100

    maps only

--- custom.js
+++ custom.js
@@ -46,9 +46,14 @@ waitForElm(selector).then((elm) => {
 waitForElm(selector2).then((elm) => {
   const aElement = document.createElement('a');
 
+  const button = `
+    <span>Maps</span>
+    <span class="UH8bI z1asCe" style="height:20px;line-height:20px;width:20px">
+      <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"></path></svg>
+    </span>`;
   aElement.classList.add('new-open-maps-btn');
   aElement.href = getAddressURL();
-  aElement.innerHTML = 'Open in Maps';
+  aElement.innerHTML = button;
 
   elm.parentElement.appendChild(aElement);
 });
